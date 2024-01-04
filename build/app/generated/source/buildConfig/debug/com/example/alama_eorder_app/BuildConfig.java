1c9b9934bbcb5bede5514aecf124624e727e7581
Wichnesh <112880628+Wichnesh@users.noreply.github.com>
2024-01-04 19:59:06 +0530

    nnn

---
+++ build/app/generated/source/buildConfig/debug/com/example/alama_eorder_app/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.alama_eorder_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.alama_eorder_app";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.7";
+}
---
+++ build/app/generated/source/buildConfig/release/com/example/alama_eorder_app/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.alama_eorder_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.alama_eorder_app";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.7";
+}
